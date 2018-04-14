a977855fe6e5a05f924babbdc373a43f191b45be
SeanCCarter <seancarter26@gmail.com>
2018-04-14 14:56:12 -0400

    Things.

--- freecad_test.py
+++ freecad_test.py
@@ -16,12 +16,29 @@ sys.path.append('/usr/lib/freecad/lib')
 
 import FreeCAD, Part, Units
 
-tu = Units.parseQuantity
+# tu = Units.parseQuantity
 
-doc = FreeCAD.newDocument()
+# doc = FreeCAD.newDocument()
 
-box = doc.addObject("Part::Box", "myBox")
+# box = doc.addObject("Part::Box", "myBox")
 
-doc.recompute()
+# doc.recompute()
+
+# b = Part.makeBox(10,10,10)
+# b.exportStl("box.stl")
+
+
+import Mesh, BuildRegularGeoms
+sphere = Mesh.Mesh( BuildRegularGeoms.Sphere(5.0, 50) )
+cylinder = Mesh.Mesh( BuildRegularGeoms.Cylinder(2.0, 10.0, True, 1.0, 50) )
+diff = sphere
+diff = diff.difference(cylinder)
+d = FreeCAD.newDocument("Sphereender")
+d.addObject("Mesh::Feature","Diff_Sphere_Cylinder").Mesh=sphere
+d.recompute()
+
+objects = []
+objects.append(FreeCAD.getDocument("Sphereender").getObject("Diff_Sphere_Cylinder"))
+Mesh.export(objects, "/home/sean/Documents/Olin/Senior2/touchingdata/spherender.stl")
 
 print "ok"
